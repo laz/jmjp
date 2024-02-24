aa5294d1d82870b653c2b2e1829ff633587ee8db
laz <danier123@gmail.com>
2024-02-24 22:23:06 +0900

    chalk으로 로그 꾸미기~~

--- src/server/utils/parseCorpCode.ts
+++ src/server/utils/parseCorpCode.ts
@@ -4,11 +4,12 @@ import path from 'node:path';
 import AdmZip from 'adm-zip';
 import fetchCorpCode from '../fetches/fetchCorpCode';
 
+import chalk from 'chalk';
+
 const CORP_CODE_PATH = path.join(__dirname, process.env.CORP_CODE_PATH ?? '');
 
 /**
  * 고유번호 전체 목록 파일 저장
- * TODO: 에러 처리?
  */
 const writeCorpCodeToDisk = (corpCodeZipFile: AdmZip) => {
   corpCodeZipFile.extractAllTo(CORP_CODE_PATH);
@@ -49,20 +50,21 @@ export const getCorpCode = async () => {
 
   // 파일 존재하는지 체크
   try {
+    console.log(chalk.greenBright('[CORPCODE]'), 'Reading corpcode from disk...');
     corpCode = readCorpCodeFromDisk();
+    console.log(chalk.greenBright('[CORPCODE]'), 'Read corpcode from disk.');
   } catch {
-    // TODO: chalk
-    console.info('Corp code file does not exists.');
-
     // 없다면 새로 파일 가져오기
+    console.log(chalk.blueBright('[CORPCODE]'), 'Corpcode does not exist in disk. Fetching corpcode...');
+
     try {
       const corpCodeArrayBuffer = await fetchCorpCode();
       corpCode = await parseAndWriteCorpCode(corpCodeArrayBuffer);
+      console.log(chalk.greenBright('[CORPCODE]'), 'Fetched corpcode.');
     } catch (error) {
-      console.info('Fetching corp code failed.');
+      // TODO: 다 실패한 경우 확인 필요
+      console.log(chalk.red('[CORPCODE]'), 'Fetching corpcode failed!');
       console.error(error);
-
-      // TODO: 다 실패한 경우 어떻게 처리할까?
     }
   }
 
